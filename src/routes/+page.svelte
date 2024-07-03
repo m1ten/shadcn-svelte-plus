ed1724a28ebf61a45ca7151c82275a8469c68daa
fourarmy <57693631+m1ten@users.noreply.github.com>
2024-07-03 16:27:47 -0400

    modifier variant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
-  import { Button } from '@/components/ui/button';
-  import { ArrowLeft, ArrowRight } from 'lucide-svelte';
+    import { Button } from "@/components/ui/button";
+    import { ArrowLeft, ArrowRight } from "lucide-svelte";
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen space-y-4">
-  <Button variant="expandIcon" icon={ArrowLeft} iconPosition="left"
-    >l-test
-  </Button>
-  <Button variant="expandIcon" icon={ArrowRight} iconPosition="right"
-    >r-test
-  </Button>
+    <Button
+        variant="secondary"
+        modifier="expandIcon"
+        icon={ArrowLeft}
+        iconPosition="left"
+        >l-test
+    </Button>
+    <Button
+        variant="link"
+        modifier="shine"
+        icon={ArrowRight}
+        iconPosition="right"
+        >r-test
+    </Button>
 </div>
